51250f30dbd0f3ce04df08aa381ee1472c106ffc
Shakil-1501 <63920152+Shakil-1501@users.noreply.github.com>
2020-07-19 18:53:42 +0530

    Update test.py

--- test.py
+++ test.py
@@ -13,7 +13,7 @@ from matplotlib import pyplot as plt
 # Argument Parser
 parser = argparse.ArgumentParser(description='High Quality Monocular Depth Estimation via Transfer Learning')
 parser.add_argument('--model', default='nyu.h5', type=str, help='Trained Keras model file.')
-parser.add_argument('--input', default='examples/*.png', type=str, help='Input filename or folder.')
+parser.add_argument('--input', default='examples/*.jpg', type=str, help='Input filename or folder.')
 args = parser.parse_args()
 
 # Custom object needed for inference and training
@@ -40,5 +40,5 @@ outputs = predict(model, inputs)
 viz = display_images(outputs.copy(), inputs.copy())
 plt.figure(figsize=(10,5))
 plt.imshow(viz)
-plt.savefig('test.png')
+plt.savefig('test.jpg')
 plt.show()
